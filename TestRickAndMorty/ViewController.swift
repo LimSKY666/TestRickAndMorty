f724162b86e388dd6d26d76b30d03813d878c4b2
Semyon Sokolov <simionsimion@yandex.ru>
2022-04-23 13:09:45 +0300

    Initial Commit

---
+++ TestRickAndMorty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestRickAndMorty
+//
+//  Created by Семён Соколов on 23.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
